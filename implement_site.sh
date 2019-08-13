40faaedd255f18bc416a0bdd75a1cce0a15b2a19
Venky Chinnadurai <Chinnadurai.Venky@gmail.com>
2019-08-13 14:45:36 +0000

    Cleanup after recreating the Ansible Master

--- implement_site.sh
+++ implement_site.sh
@@ -1,7 +1,8 @@
 
-export ANSIBLE_HOSTS=/home/testuser/AnsibleCapstone/inventory
+export ANSIBLE_CONFIG=/home/ansible/ansible_capstone
+export ANSIBLE_HOSTS=/home/ansible/ansible_capstone/inventory
 
-export EC2_INI_PATH=/home/testuser/AnsibleCapstone/inventory/ec2.ini 
+export EC2_INI_PATH=/home/ansible/ansible_capstone/inventory/ec2.ini 
 
-ansible-playbook -i ./inventory --ask-vault-pass --key-file ~/.ssh/AnsibleCapstoneKeyPair.pem site.yml
+ansible-playbook -i ./inventory --ask-vault-pass --key-file ~/.ssh/AnsibleCapstoneKeyPair2.pem site.yml --check
 #ansible-playbook -i ./inventory/hosts --ask-vault-pass dbserver.yml 
